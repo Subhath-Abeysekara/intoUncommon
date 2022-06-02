e07a729a34896f540edc7be2f0b6ee82abd200c4
ZUH <subath.abeysekara@gmail.com>
2022-06-02 01:48:02 -0800

    change Files

--- src/main/java/com/intouncommon/backend/Repository/producerCategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.intouncommon.backend.Repository;
-
-import com.intouncommon.backend.Entity.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface producerCategoriesRepository extends JpaRepository<producerCategories, Long> {
-}
