58bf790af89fef01813e06ba295413cc855dde29
ZUH <subath.abeysekara@gmail.com>
2022-07-04 04:12:30 -0800

    postgresql

--- src/main/java/com/intouncommon/backend/Entity/productions.java
+++ src/main/java/com/intouncommon/backend/Entity/productions.java
@@ -1,5 +1,6 @@
 package com.intouncommon.backend.Entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.ToString;
 
