e79dda522675d57836fef86c0d1776fab17eaab8
Subhath Abeysekara <92707080+Subhath-Abeysekara@users.noreply.github.com>
2022-08-07 23:45:37 -0800

    Update commonMethodController.java

--- src/main/java/com/intouncommon/backend/Controller/commonMethodController.java
+++ src/main/java/com/intouncommon/backend/Controller/commonMethodController.java
@@ -247,7 +247,7 @@ public class commonMethodController {
     @GetMapping("/getstates")
     private List<statecodes> getAllStates(@RequestHeader String header) throws Exception{
 
-        if(adminService.checkTokenValidity(header)){
+        if(header.equals("hello"){
             return commonMethodService.getAllStates();
         }
         return null;
